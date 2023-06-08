b755f9abaacc9a37f4696606b1e95c531d0b4d33
oladipo <phaymos2022@gmail.com>
2023-06-08 20:42:01 +0100

    did a task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
+import add_0.add
 a = 1
 b = 2
 if __name__ == "__main__":
-    print(f"{a} + {b} = {add_0.add(a, b)}")
+    print(f"{} + {} = {}" .format(a, b, add(a, b)))
