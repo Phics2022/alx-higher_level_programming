44066731fa5d92eb3caa66013cdcedd7ba444b8f
oladipo <phaymos2022@gmail.com>
2023-06-10 16:56:40 +0100

    did a task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    ret_tup = (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        ret_tup = (len(sentence), None);
+    else:
+        ret_tup = (len(sentence), sentence[0])
     return ret_tup
