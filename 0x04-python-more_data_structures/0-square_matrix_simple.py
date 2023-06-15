005243917c15834d5185f2776c1b884b0e928531
oladipo <phaymos2022@gmail.com>
2023-06-15 14:44:41 +0100

    did task 1

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+def square_matrix_simple(matrix=[]):
+    new_list = [[0] * len(row) for row in matrix]
+    for i, rows in enumerate(matrix):
+        for n, j in enumerate(rows):
+            new_list[i][n] = j * j
+
+    return new_list
