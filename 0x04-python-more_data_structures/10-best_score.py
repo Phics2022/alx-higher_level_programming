b72f887ad57340f315a734ec0de2cef75fccdc41
oladipo <phaymos2022@gmail.com>
2023-06-15 17:59:21 +0100

    added another conditional statement

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     score = 0
     key = ""
-    if len(a_dictionary) == 0 or a_dictionary is None:
+    if a_dictionary is None or len(list(a_dictionary)) == 1:
         return None
     for y, i in a_dictionary.items():
         if i > score:
