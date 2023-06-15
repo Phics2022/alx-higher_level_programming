b79aec811cd0937c38a12d05a46e929024cb2d54
oladipo <phaymos2022@gmail.com>
2023-06-15 17:46:51 +0100

    did task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    score = 0
+    key = ""
+    if a_dictionary is None:
+        return None
+    for y, i in a_dictionary.items():
+        if i > score:
+            score = i
+            key = y
+    return key
