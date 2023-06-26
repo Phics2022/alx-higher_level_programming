3cdc79d8daca8b85fb300819442cfa6f0d90d5db
oladipo <phaymos2022@gmail.com>
2023-06-26 17:47:13 +0100

    removed whitespace between str and format

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for y in range(x):
         try:
-            print("{:d}" .format(my_list[y]), end="")
+            print("{:d}".format(my_list[y]), end="")
         except (ValueError, TypeError):
             pass
         else:
