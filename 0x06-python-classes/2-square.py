71523bce77db05e9bb2a7c57e9646d75c248f799
oladipo <phaymos2022@gmail.com>
2023-06-29 07:45:10 +0100

    did task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module creates a class Squares with some abilities"""
+
+
+class Square:
+    """This class creates a square"""
+    def __init__(self, size=0):
+        """ initialise the size of the square
+        Args:
+            size: size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
