1ff46766c61ccb9cb72ae9a9808bdb625b75a046
oladipo <phaymos2022@gmail.com>
2023-07-06 10:13:10 +0100

    did task 4 with all the necsary test

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" this moddule does text indentation """
+
+
+def text_indentation(text):
+    """ text indentation module
+    Args:
+    text(str): string
+
+    Raise:
+    TypeError: when text is not a string
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    ind = [".", "?", ":"]
+    lines = text.splitlines()
+    for x, line in enumerate(lines):
+        line = line.strip()
+        line = line.replace(". ", ".")
+        line = line.replace("? ", "?")
+        line = line.replace(": ", ":")
+        for char in ind:
+            line = line.replace(char, char + "\n\n")
+
+        print(line)
