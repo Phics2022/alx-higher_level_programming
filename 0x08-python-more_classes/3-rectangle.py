d43828275c9d29a72c636c468fd225f5f63789ba
oladipo <phaymos2022@gmail.com>
2023-07-07 02:10:59 +0100

    re did task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -63,6 +63,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         rec = ""
-        for i in range(self.__height):
+        for i in range(self.__height - 1):
             rec += "#" * self.__width + "\n"
+        rec += "#" *self.__width
         return rec
