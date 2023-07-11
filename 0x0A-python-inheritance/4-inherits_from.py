65564e19a1141bfca0b3a2ddcc898615350d78bd
oladipo <phaymos2022@gmail.com>
2023-07-11 03:00:40 +0100

    did task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" this module check for subclasses"""
+
+
+def inherits_from(obj, a_class):
+    """ check if an object is ..."""
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
