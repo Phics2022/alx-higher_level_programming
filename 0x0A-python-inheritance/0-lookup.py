091200113328e8f0cd09b7bee3d601e961a9065b
oladipo <phaymos2022@gmail.com>
2023-07-11 02:04:29 +0100

    did task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Attributes and methods """
+
+
+def lookup(obj):
+    """ gets the methods and attributes of an object
+    Args:
+    obj(object): the object to check out
+
+    Return:
+    A list of the objects and attributes
+    """
+    attr = dir(obj)
+    lister = [i for i in obj]
+    return lister
