c2a19b56ab1f2140665ea7ba1b6d40895482dd40
oladipo <phaymos2022@gmail.com>
2023-07-13 02:33:33 +0100

    did task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module returns the json rep of an object"""
+
+
+import json
+def to_json_string(my_obj):
+    """ turn`my_obj` to a json representation"""
+    x = json.dumps(my_obj)
+    return x
