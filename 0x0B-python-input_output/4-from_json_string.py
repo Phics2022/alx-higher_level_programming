8dc0d0baf530943a2ca74ace31c818338254e8c9
oladipo <phaymos2022@gmail.com>
2023-07-13 02:39:14 +0100

    did task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" from json to object """
+
+
+import json
+
+
+def from_json_string(my_str):
+    """ turn `my_str` to an object
+    Args:
+    my_str
+    """
+    x = json.load(my_str);
+    return x
