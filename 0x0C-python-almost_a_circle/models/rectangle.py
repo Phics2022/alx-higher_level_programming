44c90881cfcde69c2ed7c41d4d40ef169eb0aeaa
oladipo <phaymos2022@gmail.com>
2023-07-18 04:15:26 +0100

    did task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,3 +107,16 @@ class Rectangle(Base):
         """overrites the str prop"""
         string = "[Rectangle] ({}) {}/{} - {}/{}"
         return string.format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        """ updates the values"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
