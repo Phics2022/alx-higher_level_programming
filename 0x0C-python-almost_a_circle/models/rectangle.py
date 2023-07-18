bf1c9ea4b544add70abd0f93050cd4b28fec95de
oladipo <phaymos2022@gmail.com>
2023-07-18 02:21:09 +0100

    didn task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" creates a rectangle class"""
+
+from base import Base
+
+class Rectangle(Base):
+    """ creates a rectangle object
+    and it inherits from Base class
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """returns the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """returns the width"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """returns the width"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """returns the width"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,18 @@
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    def test_id(self):
+        r1 = Rectangle(2, 10)
+        r2 = Rectangle(10, 2)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r3.id, 12)
+
+    def test_value_getter(self):
+        rec = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(rec.x(), 0)
+        self.assertEqual(rec.width(), 10)
+        self.assertEqual(rec.height(), 2)
