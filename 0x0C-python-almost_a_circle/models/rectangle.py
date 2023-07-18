0031470e4373e52d2991204b65d1bfd5056c909e
oladipo <phaymos2022@gmail.com>
2023-07-18 02:22:33 +0100

    documented my __init__ function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ creates a rectangle class"""
 
+
 from base import Base
+
 
 class Rectangle(Base):
     """ creates a rectangle object
@@ -9,6 +11,7 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ initailises the class"""
         super().__init__(id)
         self.__width = width
         self.__height = height
