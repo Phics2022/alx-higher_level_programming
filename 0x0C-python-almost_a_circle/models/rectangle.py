3870a978a95be84d7171accf72764d23bb17ab78
oladipo <phaymos2022@gmail.com>
2023-07-19 01:32:41 +0100

    did task 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,13 +110,11 @@ class Rectangle(Base):
 
     def update(self, *args):
         """ updates the values"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.__width = args[1]
-        if len(args) >= 3:
-            self.__height = args[2]
-        if len(args) >= 4:
-            self.__x = args[3]
-        if len(args) >= 5:
-            self.__y = args[4]
+        if len(args) > 0:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i, value in enumerate(args):
+                if i < len(attrs):
+                    setattr(self, attrs[i], value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
