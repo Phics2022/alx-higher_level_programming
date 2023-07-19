155ef349806b1a90b4636cefca7b0ce59459948f
oladipo <phaymos2022@gmail.com>
2023-07-19 01:22:02 +0100

    i task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -113,10 +113,10 @@ class Rectangle(Base):
         if len(args) >= 1:
             self.id = args[0]
         if len(args) >= 2:
-            self.width = args[1]
+            self.__width = args[1]
         if len(args) >= 3:
-            self.height = args[2]
+            self.__height = args[2]
         if len(args) >= 4:
-            self.x = args[3]
+            self.__x = args[3]
         if len(args) >= 5:
-            self.y = args[4]
+            self.__y = args[4]
