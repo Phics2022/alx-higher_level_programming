0591776ff9868e2db3bec2e559ee9520019022b7
oladipo <phaymos2022@gmail.com>
2023-07-20 01:55:46 +0100

    did task 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -51,3 +51,14 @@ class Square(Rectangle):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """ return a dictionary representation
+        of the square object
+        """
+        return {
+            'id': self.id,
+            'size': self.width,
+            'x': self.x,
+            'y': self.y
+        }
