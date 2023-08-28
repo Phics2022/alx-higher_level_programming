8541c21140361b336025c962a5420c3efe6dadb5
Oluwaferanmi Oladipo <122922115+Phics2022@users.noreply.github.com>
2023-08-28 16:07:28 +0100

    Delete oxob

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" this module appends a text to a file"""
-
-
-def append_write(filename="", text=""):
-    """appends `text` to a file `filename`"""
-    with open(filename, 'a', encoding="utf-8") as f:
-        byte_appended = f.write(text)
-        return byte_appended
