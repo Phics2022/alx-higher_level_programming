87a51d89beaabb877054c69b2c4a1ad7519b4067
Oluwaferanmi Oladipo <122922115+Phics2022@users.noreply.github.com>
2023-08-28 16:06:30 +0100

    Delete oxoa

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" This module inherits from list """
-
-
-class MyList(list):
-    """ print a list """
-    def print_sorted(self):
-        print(sorted(self))
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" creates a geometry"""
-
-
-class BaseGeometry:
-    """create a base geometry"""
-    pass
