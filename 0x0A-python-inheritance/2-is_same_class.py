e80fc8e341efe01fd4053bebfdcc126d5315d0c2
oladipo <phaymos2022@gmail.com>
2023-09-11 23:12:40 +0100

    did task 2: check if an object is an instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Write a function that returns True if the object
+s exactly an instance of the specified class ;
+otherwise False.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    checks if an object is an instance of a class
+
+    Args:
+    obj: The object to check
+    a_class: The class 
+
+    Return:
+    True(Bool): if obj is an instance of a_class
+    False(Bool): if obj is not an instance of a_class
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
