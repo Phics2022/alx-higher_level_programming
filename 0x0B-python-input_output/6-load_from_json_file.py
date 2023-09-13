7ad134fa3bdf6c0afb36ee20323f434d215b2099
oladipo <phaymos2022@gmail.com>
2023-09-13 15:54:40 +0100

    did task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module loads json from file"""
+import json
+
+
+def load_from_json_file(filename):
+    """ create abject from `filename`"""
+
+    with open(filename, "r", encoding="utf-8") as file:
+        buff = json.load(filaname)
+    return buff
