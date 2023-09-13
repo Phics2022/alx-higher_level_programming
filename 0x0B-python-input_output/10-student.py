a606b4c209b0bf0a3a63ad5f11ac506a6319c327
oladipo <phaymos2022@gmail.com>
2023-09-13 23:16:53 +0100

    did task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""student to json"""
+
+
+class Student:
+    """ a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """ initialise the class"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves the dictionary rep of instance"""
+
+        if attrs is None:
+            serialised = {
+                    'first_name': self.first_name,
+                    'last_name': self.last_name,
+                    'age': self.age}
+            return serialised
+        else:
+            serialised = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    serialised[attr] = getattr(self, attr)
+                else:
+                    continue
+            return serialised
