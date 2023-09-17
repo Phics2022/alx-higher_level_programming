89f3b14eaf37ed2f6f0c37d9ee650f901f9d9ecf
oladipo <phaymos2022@gmail.com>
2023-09-17 21:53:42 +0100

    did task 4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,7 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
+
+    def area(self):
+        """ returns the area"""
+        return self.__height * self.__width
