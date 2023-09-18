93e7ba089f63234a41dc5dba6d6a98828d5ad99c
oladipo <phaymos2022@gmail.com>
2023-09-18 21:03:19 +0100

    coreected someting

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -48,7 +48,7 @@ class Square(Rectangle):
                 self.id = args[0]
             if len(args) >= 2:
                 self.width = args[1]
-                self.height = args[2]
+                self.height = args[1]
             if len(args) >= 3:
                 self.x = args[2]
             if len(args) >= 4:
